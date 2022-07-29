2ac3aa9081a33e91d433a90865e4645f3e53a20a
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-29 21:44:11 +0530

    Maximum String Value

--- src/com/bridgelabz/GenericsMain.java
+++ src/com/bridgelabz/GenericsMain.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class GenericsMain {
     public static void main(String[] args) {
 
-        MaxValue maxFloat = new MaxValue();
-        maxFloat.maxValue(32.63F, 54.721F, 72.539F);
+        MaxValue maxString = new MaxValue();
+        maxString.maxValue("Hello", "Abhijeet", "Sunday");
     }
 }
--- src/com/bridgelabz/MaxValue.java
+++ src/com/bridgelabz/MaxValue.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class MaxValue {
 
-    void maxValue(Float num1, Float num2, Float num3) {
+    void maxValue(String num1, String num2, String num3) {
 
         if ((num1.compareTo(num2)) >= 0 && (num1.compareTo(num3)) >= 0)
             System.out.println(num1 + " is the largest number");
