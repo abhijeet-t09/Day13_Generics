313979d3da51f7e9122f180b4c967419314cfb9d
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-29 22:00:14 +0530

    Generics Method

--- src/com/bridgelabz/GenericsMain.java
+++ src/com/bridgelabz/GenericsMain.java
@@ -3,7 +3,17 @@ package com.bridgelabz;
 public class GenericsMain {
     public static void main(String[] args) {
 
-        MaxValue maxString = new MaxValue();
-        maxString.maxValue("Hello", "Abhijeet", "Sunday");
+        MaxValue maxVal = new MaxValue();
+        System.out.println("Integer Test Case");
+        maxVal.maxValue(35, 124, 327);
+        System.out.println();
+
+        System.out.println("Float Test Case");
+        maxVal.maxValue(45.32F, 45.53F, 325.12F);
+        System.out.println();
+
+        System.out.println("String Test Case");
+        maxVal.maxValue("Dog", "Tiger", "Antelope");
+        System.out.println();
     }
 }
--- src/com/bridgelabz/MaxValue.java
+++ src/com/bridgelabz/MaxValue.java
@@ -2,15 +2,15 @@ package com.bridgelabz;
 
 public class MaxValue {
 
-    void maxValue(String num1, String num2, String num3) {
+    static <K extends Comparable<K>>void maxValue(K val1, K val2, K val3) {
 
-        if ((num1.compareTo(num2)) >= 0 && (num1.compareTo(num3)) >= 0)
-            System.out.println(num1 + " is the largest number");
+        if ((val1.compareTo(val2)) >= 0 && (val1.compareTo(val3)) >= 0)
+            System.out.println(val1 + " is the largest");
 
-        else if ((num2.compareTo(num1)) >= 0 && (num2.compareTo(num3)) >= 0) {
-            System.out.println(num2 + " is largest");
+        else if ((val2.compareTo(val1)) >= 0 && (val2.compareTo(val3)) >= 0) {
+            System.out.println(val2 + " is largest");
         } else {
-            System.out.println(num3 + " is largest");
+            System.out.println(val3 + " is largest");
         }
     }
 }
